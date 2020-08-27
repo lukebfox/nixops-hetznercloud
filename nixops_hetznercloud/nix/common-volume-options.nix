a13b4b0043d9b0094cbcfdfda9303beebe03c512
Luke Bentley-Fox <git@lukebentleyfox.net>
2020-08-27 15:05:04 +0100

    ignore profiles

---
+++ nixops_hetznercloud/nix/common-volume-options.nix
@@ -0,0 +1,13 @@
+# Options shared between the Volume resource type and the
+# deployment.hetznercloud.blockDeviceMapping/fileSystems.*.hetznercloud
+# options in Hetzner Cloud instances.
+
+{ config, lib, ... }:
+
+with lib;
+
+{
+  options = {
+    
+  };
+}
