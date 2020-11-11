f19e727034a1190a3d9ceeb3ffcb8aea5b8eafac
Luke Bentley-Fox <git@lukebentleyfox.net>
2020-11-11 13:59:31 +0000

    deflake

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
-(import (
-  fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/c75e76f80c57784a6734356315b306140646ee84.tar.gz";
-    sha256 = "071aal00zp2m9knnhddgr2wqzlx6i6qa1263lv1y7bdn2w20h10h"; }) {
-      src =  ./.;
-}).defaultNix
+{ pkgs ? import <nixpkgs> {} }:
+let
+  overrides = import ./overrides.nix { inherit pkgs; };
+in pkgs.poetry2nix.mkPoetryApplication {
+  projectDir = ./.;
+  overrides = pkgs.poetry2nix.overrides.withDefaults overrides;
+}
--- shell.nix
+++ shell.nix
@@ -1,6 +1,12 @@
-(import (
-  fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/c75e76f80c57784a6734356315b306140646ee84.tar.gz";
-    sha256 = "071aal00zp2m9knnhddgr2wqzlx6i6qa1263lv1y7bdn2w20h10h"; }) {
-      src =  ./.;
-}).shellNix.default
+{ pkgs ? import <nixpkgs> {} }:
+let
+  overrides = import ./overrides.nix { inherit pkgs; };
+in pkgs.mkShell {
+  buildInputs = [
+    (pkgs.poetry2nix.mkPoetryEnv {
+      projectDir = ./.;
+      overrides = pkgs.poetry2nix.overrides.withDefaults overrides;
+    })
+    pkgs.poetry
+  ];
+}
