74e63785e646c28a3f63145421242984e1d8d2d8
Luke Bentley-Fox <git@lukebentleyfox.net>
2020-12-06 13:37:15 +0000

    update deps

--- overrides.nix
+++ overrides.nix
@@ -1,6 +1,13 @@
 { pkgs }:
 
 self: super: {
+
+  # https://github.com/nix-community/poetry2nix/issues/218
+  packaging = super.packaging.overridePythonAttrs({buildInputs ? [], ...}: {
+    format = "pyproject";
+    buildInputs = buildInputs ++ [ self.flit-core ];
+  });
+
   nixops = super.nixops.overridePythonAttrs({ nativeBuildInputs ? [], ... }: {
     format = "pyproject";
     nativeBuildInputs = nativeBuildInputs ++ [ self.poetry self.typeguard ];
