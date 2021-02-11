b998ecc8782d3f54b0487206e7ae1b82e63a4dbe
Luke Bentley-Fox <git@lukebentleyfox.net>
2021-02-11 22:31:34 +0000

    more readable code

--- nixops_hetznercloud/nix/hetznercloud.nix
+++ nixops_hetznercloud/nix/hetznercloud.nix
@@ -135,7 +135,12 @@ in
 
     deployment.hetznerCloud.volumes = mkOption {
       default = [ ];
-      #example = [ { volume = "volume1"; } ]; TODO
+      example = literalExample ''
+        [{
+          volume = "volume1";
+          mountPoint = "/mnt";
+        }];
+        '';
       type = with types; listOf (submodule hetznerCloudDiskOptions);
       description = ''
         List of attached Volumes. ``fileSystems`` attributes which contain
@@ -177,7 +182,7 @@ in
   config = mkIf (config.deployment.targetEnv == "hetznercloud") {
 
     assertions = [
-      { assertion = 3 >= length config.deployment.hetznerCloud.serverNetworks;
+      { assertion = length config.deployment.hetznerCloud.serverNetworks <= 3;
         message = "Hetzner Cloud Servers can only attach to up to three networks at once.";
       }
     ];
